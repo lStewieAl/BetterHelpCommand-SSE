e3f7b597ee17972113337517aaaadcd4fbc877e5
lStewieAl <lStewieAl@gmail.com>
2022-11-30 12:40:14 +0000

    Allow NPC in place of NPC_; print usage if no filter is provided

--- src/plugin/main.cpp
+++ src/plugin/main.cpp
@@ -6,7 +6,9 @@ extern "C" const std::uintptr_t SkipAddr = ScriptHelpFunctionAddr + 0x943;
 extern "C" const std::uintptr_t ContinueAddr = ScriptHelpFunctionAddr + 0x823;
 inline const std::uintptr_t TypeIDHookAddr = ScriptHelpFunctionAddr + 0x81D;
 inline const std::uintptr_t ViewerStringsAddr = ScriptHelpFunctionAddr + 0x665;
-inline const std::uintptr_t HelpUsageAddr = ScriptHelpFunctionAddr + 0xA43;
+inline const std::uintptr_t OtherFormsPrintAddr = ScriptHelpFunctionAddr + 0x746;
+inline const std::uintptr_t HelpUsageAddr1 = ScriptHelpFunctionAddr + 0xA57;
+inline const std::uintptr_t HelpUsageAddr2 = ScriptHelpFunctionAddr + 0xA6A;
 inline const std::uintptr_t ParamInfoToReplaceAddr = REL::ID(365802).address();
 inline const std::uintptr_t TwoOptionalStringsAddr = REL::ID(368114).address();
 inline const std::uintptr_t FormTypeStringsAddr = REL::ID(359121).address();
@@ -25,37 +27,68 @@ void __cdecl OnHelpExtractArgs(const RE::SCRIPT_PARAMETER* a_paramInfo, RE::SCRI
 	filterType = strtoul(typeStr, nullptr, 10);
 	if (!filterType)
 	{
-		auto formTypes = (const char**)FormTypeStringsAddr;
-		// search the form types enum for the identifier
-		for (int i = 0; i <= 136; ++i)
+		if (!_stricmp(typeStr, "NPC"))
 		{
-			if (!_stricmp(typeStr, formTypes[3 * i]))
+			filterTypeID = static_cast<unsigned char>(RE::FormType::NPC);
+			filterType = 4; // set to OTHER FORMS only
+		}
+		else
+		{
+			auto formTypes = (const char**)FormTypeStringsAddr;
+			// search the form types enum for the identifier
+			for (int i = 0; i <= 136; ++i)
 			{
-				filterTypeID = static_cast<unsigned char>(i);
-				filterType = 4; // set to OTHER FORMS only
-				break;
+				if (!_stricmp(typeStr, formTypes[3 * i]))
+				{
+					filterTypeID = static_cast<unsigned char>(i);
+					filterType = 4; // set to OTHER FORMS only
+					break;
+				}
 			}
 		}
 	}
 	*(DWORD*)typeStr = filterType;
 }
 
+void __fastcall OnPrintOtherFormsString(RE::ConsoleLog* consoleLog, const char* otherFromsString)
+{
+	if (!filterTypeID)
+	{
+		consoleLog->Print(otherFromsString);
+	}
+}
+
+void __fastcall OnPrintUsageHelpString(RE::ConsoleLog* consoleLog, const char* otherFromsString)
+{
+	if (!filterTypeID)
+	{
+		consoleLog->Print(otherFromsString);
+		consoleLog->Print("filters: 0-all 1-functions, 2-settings, 3-globals, 4-other forms, CELL-cells, NPC_-npcs etc.");
+	}
+}
+
 SKSEPluginLoad(const SKSE::LoadInterface* a_skse)
 {
 	SKSE::Init(a_skse);
-	SKSE::AllocTrampoline(0x20);
-	auto& trampoline = SKSE::GetTrampoline();
-	trampoline.write_call<5>(ExtractArgsHookAddr, UInt64(OnHelpExtractArgs));
-	trampoline.write_branch<5>(TypeIDHookAddr, UInt64(HookCheckTypeID));
 
-	// allow two strings instead of string + int
-	REL::safe_write(ParamInfoToReplaceAddr, TwoOptionalStringsAddr);
+	if (*(long long*)TypeIDHookAddr == 0x8B4800000120840F)
+	{
+		SKSE::AllocTrampoline(0x40);
+		auto& trampoline = SKSE::GetTrampoline();
+		trampoline.write_call<5>(ExtractArgsHookAddr, OnHelpExtractArgs);
+		trampoline.write_call<5>(OtherFormsPrintAddr, OnPrintOtherFormsString);
+		trampoline.write_call<5>(HelpUsageAddr1, OnPrintUsageHelpString);
+		trampoline.write_branch<5>(TypeIDHookAddr, HookCheckTypeID);
 
-	// remove useless 'VIEWER STRINGS NOT AVAILABLE' print
-	REL::safe_write(ViewerStringsAddr, "\x0f\x1f\x44\x00\x00", 5);
+		// allow two strings instead of string + int
+		REL::safe_write(ParamInfoToReplaceAddr, TwoOptionalStringsAddr);
 
-	// remove prints about help command usage
-	REL::safe_write(HelpUsageAddr, "\xEB\x2A", 2);
+		// remove useless 'VIEWER STRINGS NOT AVAILABLE' print
+		REL::safe_write(ViewerStringsAddr, "\x0f\x1f\x44\x00\x00", 5);
+
+		// remove prints about help command usage
+		REL::safe_fill(HelpUsageAddr2, 0x90, 5);
+	}
 
 	return true;
 }
